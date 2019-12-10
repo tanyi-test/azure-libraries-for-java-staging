cfd357f18c4cc143c7ceb569826d86c56d8cb41d
Tanyi Chen <tanchen@microsoft.com>
2019-12-10 15:37:46 +0800

    feat: add staging pom

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,23 @@
+package com.mycompany.app;
+
+import java.io.File;
+
+import com.microsoft.azure.management.Azure;
+import com.microsoft.azure.management.resources.fluentcore.arm.Region;
+import com.microsoft.rest.LogLevel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        final File credFile = new File(System.getenv("AZURE_AUTH_LOCATION"));
+        Azure azure = Azure.configure().withLogLevel(LogLevel.NONE).authenticate(credFile).withDefaultSubscription();
+
+        final String rgName = "test";
+        final Region region = Region.US_WEST;
+
+        azure.resourceGroups().define(rgName).withRegion(region).create();
+    }
+}
